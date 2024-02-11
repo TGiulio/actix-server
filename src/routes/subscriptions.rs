03d99edf0bfe13707c730bc086fdb2007988f795
Giulio Toldo <giulio.toldo@gmail.com>
2024-02-11 13:50:02 +0100

    add yaml files to giignore

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,14 +11,17 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
     match sqlx::query!(
-        r#"INSERT into public.subscriptions (id, email, name ,subscribed_at) VALUES ($1, $2, $3, $4)"#,
+        r#"INSERT into public.subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
-    ).execute(db_pool.get_ref()).await{
+    )
+    .execute(db_pool.get_ref())
+    .await
+    {
         Ok(_res) => HttpResponse::Ok(),
-        Err(err) => { 
+        Err(err) => {
             println!("failed to execute query: {}", err);
             HttpResponse::InternalServerError()
         }
