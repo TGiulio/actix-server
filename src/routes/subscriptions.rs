b19fee245de5416ad14401a83c24528484629985
TGiulio <giulio.toldo@gmail.com>
2024-09-16 21:20:02 +0200

    add errors for confirmation and chore refactor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,96 +1,14 @@
 use crate::{
     domain::{NewSubscriber, SubscriberEmail, SubscriberName, SubscriptionToken},
     email_client::EmailClient,
+    errors::{CheckSubError, StoreTokenError, SubscribeError},
     startup::ApplicationBaseUrl,
 };
-use actix_web::{http::StatusCode, web, HttpResponse, ResponseError, Result};
+use actix_web::{web, HttpResponse, Result};
 use anyhow::Context;
 use chrono::Utc;
 use sqlx::{PgPool, Postgres};
 use uuid::Uuid;
-
-fn error_chain_fmt(e: &impl std::error::Error, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    writeln!(f, "{} \n", e)?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, " caused by: \n\t {}", cause)?;
-        current = cause.source();
-    }
-    Ok(())
-}
-
-pub struct StoreTokenError(sqlx::Error);
-
-impl std::fmt::Display for StoreTokenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "a database error was encountered while \
-            trying to store a subscription token."
-        )
-    }
-}
-
-impl std::fmt::Debug for StoreTokenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl std::error::Error for StoreTokenError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(&self.0)
-    }
-}
-
-pub struct CheckSubError(sqlx::Error);
-
-impl std::fmt::Display for CheckSubError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "a database error was encountered while \
-            checking for subscriber existance."
-        )
-    }
-}
-
-impl std::fmt::Debug for CheckSubError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl std::error::Error for CheckSubError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(&self.0)
-    }
-}
-
-impl ResponseError for CheckSubError {}
-
-#[derive(thiserror::Error)]
-pub enum SubscribeError {
-    #[error("{0}")]
-    ValidationError(String),
-    #[error(transparent)]
-    UnexpectedError(#[from] anyhow::Error),
-}
-
-impl std::fmt::Debug for SubscribeError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl ResponseError for SubscribeError {
-    fn status_code(&self) -> StatusCode {
-        match self {
-            SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    }
-}
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
